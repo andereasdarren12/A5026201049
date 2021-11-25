6c76b8ae50e393ac85059187d546617fe748906a
andereasdarren12 <andereasdarren12@gmail.com>
2021-11-25 22:59:42 +0700

    tugaspweb1

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('bmi', function () {
+    return view('calculator');
+});
+Route::get('pendapatan', function () {
+    return view('pendapatan.index');
+});
+
+Route::get('/pendapatan','PendapatanController@index');
+
+Route::get('/pendapatan/tambah','PendapatanController@tambah');
+
+Route::post('/pendapatan/store','PendapatanController@store');
+
+Route::get('/pendapatan/edit/{id}','PendapatanController@edit');
+Route::post('/pendapatan/update/','PendapatanController@update');
+
+Route::get('/pendapatan/hapus/{id}','PendapatanController@hapus');
